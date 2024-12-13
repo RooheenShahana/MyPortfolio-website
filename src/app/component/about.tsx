c38dcbdde19ee51e0f9651ba39b6a000a0496c89
Rooheen Shahana <rubashafatime2016@gmail.com>
2024-12-13 18:04:30 +0500

    my portfolio website

---
+++ src/app/component/about.tsx
@@ -0,0 +1,47 @@
+
+import Image from 'next/image'
+
+import React from 'react'
+
+const About = () => {
+    return(
+        <div id="about">
+<section className="text-gray-600 body-font">
+  <div className="container mx-auto flex px-5 py-24 md:flex-row flex-col items-center">
+    <div className="lg:max-w-lg lg:w-full md:w-1/2 w-5/6 mb-10 md:mb-0 ">
+      <Image
+        className="object-cover object-center rounded"
+        alt="hero"
+        src={require("../../../public/assests/picture/image.jpeg")}
+        
+  
+      />
+    </div>
+    <div className="lg:flex-grow md:w-1/2 lg:pl-24 md:pl-16 flex flex-col md:items-start md:text-left items-center text-center">
+      <h1 className="title-font sm:text-4xl text-3xl mb-4 font-medium text-gray-900">
+        About Me
+       
+      </h1>
+      <p className="mb-5 leading-relaxed">
+        Explore my creative journey.I am Rooheen Shahana,a Graphic designer with a passion 
+        <br></br>
+        for innovative design and development.
+      </p>
+      <div className="flex justify-center">
+        <a href={"/assests/cv/mycv.pdf"}>
+        
+        
+        <button className="inline-flex text-white bg-blue-700 border-0 py-2 px-6 focus:outline-none hover:bg-blue-900 rounded text-lg">
+          Veiw Cv
+       </button>
+       </a>
+      </div>
+    </div>
+  </div>
+</section>
+
+
+        </div>
+    )
+}
+export default About
---
+++ src/app/component/skill.tsx
@@ -0,0 +1,134 @@
+import React from 'react'
+import { CiSquareCheck } from "react-icons/ci";
+const Skill = () => {
+
+return (
+
+    <div id="Skills">
+<section className="text-gray-600 body-font">
+  <div className="container px-5 py-24 mx-auto">
+    <div className="text-center mb-20">
+      <h1 className="sm:text-3xl text-2xl font-medium title-font text-gray-900 mb-4">
+        Skills
+      </h1>
+      <p className="text-base leading-relaxed xl:w-2/4 lg:w-3/4 mx-auto text-gray-500s">
+         My Skills
+      </p>
+      <div className="flex mt-6 justify-center">
+        <div className="w-16 h-1 rounded-full bg-blue-700 inline-flex" />
+      </div>
+    </div>
+    <div className="flex flex-wrap sm:-m-4 -mx-4 -mb-10 -mt-4 md:space-y-0 space-y-6">
+      <div className="p-4 w-[100%] md:w-1/3 flex flex-col text-center items-center">
+      
+       
+      </div>
+    
+      <div className="p-4 w-[100%] md:w-1/3 flex flex-col text-center items-center">
+        <div className="w-20 h-20 inline-flex items-center justify-center rounded-full bg-blue-500 text-white mb-5 flex-shrink-0">
+        <CiSquareCheck className= "text-xl font-bold"/>
+        </div>
+        <div className= "flex-grow">
+
+          <h2 className="text-gray-900 text-lg title-font font-medium">
+            HTML
+          </h2>
+          
+
+          <div className="relative h-1 w-full bg-gray-400 rounded-xl">
+            <div className="absolute bg-blue-700 h-1 rounded-xl w-[95%]"></div>
+            <p className="font-bold text-blue-700 text-right">95%</p>
+
+    
+        
+             </div>
+            
+
+             <div className="flex mt-6 justify-center">
+      
+      </div>
+    </div>
+    <div className="flex flex-wrap sm:-m-4 -mx-4 -mb-10 -mt-4 md:space-y-0 space-y-6">
+      <div className="p-4 w-[100%] md:w-1/3 flex flex-col text-center items-center">
+      
+       
+      </div>
+    
+      <div className="p-4 w-[100%] md:w-1/3 flex flex-col text-center items-center">
+        <div className="w-20 h-20 inline-flex items-center justify-center rounded-full bg-blue-500 text-white mb-5 flex-shrink-0">
+        <CiSquareCheck className= "text-xl font-bold"/>
+        </div>
+        <div className= "flex-grow">
+
+          <h2 className="text-gray-900 text-lg title-font font-medium">
+            CSS
+          </h2>
+          
+
+          <div className="relative h-1 w-full bg-gray-400 rounded-xl">
+            <div className="absolute bg-blue-700 h-1 rounded-xl w-[95%]"></div>
+            <p className="font-bold text-blue-700 text-right">95%</p>
+            
+            <div className="flex mt-6 justify-center">
+            
+
+            </div>
+      
+    </div>
+    <br></br>
+    <div className="flex flex-wrap sm:-m-4 -mx-4 -mb-10 -mt-4 md:space-y-0 space-y-6">
+      <div className="p-4 w-[100%] md:w-1/3 flex flex-col text-center items-center">
+      
+       
+      </div>
+    
+      <div className="p-4 w-[100%] md:w-1/3 flex flex-col text-center items-center">
+        <div className="w-20 h-20 inline-flex items-center justify-center rounded-full bg-blue-500 text-white mb-5 flex-shrink-0">
+        <CiSquareCheck className= "text-xl font-bold"/>
+        </div>
+        <div className= "flex-grow">
+
+          <h2 className="text-gray-900 text-lg title-font font-medium">
+            TYPESCRIPT
+          </h2>
+          
+
+          <div className="relative h-1 w-full bg-gray-400 rounded-xl">
+            <div className="absolute bg-blue-700 h-1 rounded-xl w-[80%]"></div>
+            <p className="font-bold text-blue-700 text-right">80%</p>
+
+            <div className="flex flex-wrap sm:-m-4 -mx-4 -mb-10 -mt-4 md:space-y-0 space-y-6">
+      <div className="p-4 w-[100%] md:w-1/3 flex flex-col text-center items-center">
+      
+       
+      </div>
+    
+     
+             </div>
+
+             </div>
+             </div>
+             </div>
+
+            
+
+           
+
+            
+
+             </div>
+          
+
+          </div>
+        </div>
+        </div>
+      </div>
+    </div>
+    
+  </div>
+</section>
+
+    </div>
+)
+}
+export default Skill 
